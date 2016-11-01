ec89c668013218a37fc7d9469501bab73d6c52b7
Erica Jurado <ej3535a@student.american.edu>
2016-11-01 04:06:09 -0400

    Jump search specific neighbour function added. Jump point search has bug where it will not always find path.

--- commonFunctions.py
+++ commonFunctions.py
@@ -19,3 +19,79 @@ def getNeighbours(currentNode, dimension):
 	if y<dimension-1:
 		neighbours.append((x,y+1))
 	return neighbours
+
+def getJumpNeighbours(currentNode, goal, dimension, maze):
+	x = currentNode[0]
+	y = currentNode[1]
+
+	neighbours = []
+
+	i = x+1
+	while i < dimension:
+		if maze[i][y][1] == False:
+			if (i,y) == goal:
+				return [[(i,y), 0]]
+
+			if y+1 < dimension:
+				if maze[i][y+1][1] == False:
+					neighbours.append([(i,y), i-x])
+			elif y-1 >= 0:
+				if maze[i][y-1][1] == False:
+					neighbours.append([(i,y), i-x])
+
+		else:
+			i = dimension+1
+		i += 1
+
+	i = x-1
+	while i >= 0:
+		if maze[i][y][1] == False:
+			if (i,y) == goal:
+				return [[(i,y), 0]]
+
+			if y+1 < dimension:
+				if maze[i][y+1][1] == False:
+					neighbours.append([(i,y), x-i])
+			elif y-1 >= 0:
+				if maze[i][y-1][1] == False:
+					neighbours.append([(i,y), x-i])
+		else:
+			i = -1
+		i -= 1
+
+	j = y+1
+	while j < dimension:
+		if maze[x][j][1] == False:
+			if (x,j) == goal:
+				return [[(x,j), 0]]
+
+			if x+1 < dimension:
+				if maze[x+1][j][1] == False:
+					neighbours.append([(x,j), j-y])
+			elif x-1 >= 0:
+				if maze[x-1][j][1] == False:
+					neighbours.append([(x,j), j-y])
+		else:
+			j = dimension+1
+		j +=1
+
+	j = y-1
+	while j >= 0:
+		if maze[x][j][1] == False:
+			if (x,j) == goal:
+				return [[(x,j), 0]]
+
+			if x+1 < dimension:
+				if maze[x+1][j][1] == False:
+					neighbours.append([(x,j), y-j])
+			elif x-1 >= 0:
+				if maze[x-1][j][1] == False:
+					neighbours.append([(x,j), y-j])
+		else:
+			j = -1
+		j -= 1
+
+	return neighbours
+
+
+
--- jumpPoint.py
+++ jumpPoint.py
@@ -1,54 +1,103 @@
-def JumpPoint(start, end, dimension, maze, openCells, closedCells):
-	x = start[0]
-	y = start[1]
+from commonFunctions import calculateH
+from commonFunctions import getJumpNeighbours
 
-	#openCells.append((x, y, maze[x][y]))
+def JumpPoint(start, goal, maze):
+	dimension = len(maze)
+	
+	openList = [] #set of nodes to be evaluated
+	closedList = [] #set of nodes already evaluated
 
-	i = 0
-	j = 0
+	# Hcost is the heuristic cost of a node to the goal (Manhattan distance is used).
+	Hcost = calculateH(start, goal)
+	path = []
+	start = [start, 0, Hcost, path]
 
-	print start
+	#point, g cost (distance from starting node/distance travelled), h cost (distance from end node)
+	openList.append(start)
 
-	while j < len(maze):
-		if maze[x][j] != True and j!=y:
-			if j+1 < dimension:
-				if maze[x][j+1]!= True:
-					openCells.append((x,j,maze[x][j]))
-			elif (j-1) >0:
-				if maze[x][j-1]!= True:
-					openCells.append((x,y,maze[x][j]))
-		j +=1
+	# While our openList is not empty, we still have nodes to evaluate
+	while openList:
+		print "openList" + str(openList)
+		print "closedList" + str(closedList)
+		# Arbitarily set our current node to the first on in the openList.
+	 	current = openList[0]
+	 	print "current" + str(current)
 
+	 	# This will be used to determine the new Gcost of the path for successive nodes.
+	 	currentGCost = current[1]
 
-	while i < len(maze):
-		if maze[i][y] != True and i!=x:
-			if i+1 <dimension:
-				if maze[i+1][y]!= True:
-					openCells.append((i,y,maze[i][y]))
-			elif i-1 <dimension:
-				if maze[i-1][y]!= True:
-					openCells.append((i,y,maze[i][y]))
-		i +=1
+	 	# Then loop through the list of nodes to find the node with the lowest F cost.
+	 	# The f cost is caluculate by added the g and h costs of the node in question.
+	 	for node in openList:
+	 		if (node[1]+node[2]) < (current[1]+current[2]):
+	 			current = node
 
-	minCostCell = openCells[0]
-	for cell in openCells:
-		if cell[2] < minCostCell[2]:
-			minCostCell=cell
+	 	path  = current[3]
+	 	path = path + [current[0]]
+	 	print path
 
-	print minCostCell
-	print openCells
-	print closedCells
+	 	# Then remove the node with the lowest cost from the lowest list.
+	 	openList.remove(current)
+	 	# And add this node to the closed list since we've evaluated it.
+	 	inClosed = False
+	 	for closed in closedList:
+	 		if closed[0] == current[0]:
+	 			inClosed = True
 
-	n = Nodes(3)
-	x = Nodes(4)
-	d = Nodes(5)
+	 	if inClosed == False:
+	 		closedList.append(current)
 
-	n.add_childNode(x)
-	x.add_childNode(d)
+	 	# If the current node is the goal, we know that we've found out path.
+	 	if current[0] == goal:
+	 		return path
 
-	for node in n.children:
-		print node.data
+	 	# Get the neighbours of our current node.
+	 	neighbours = getJumpNeighbours(current[0], goal, dimension,maze)
+	 	print "neighbours" + str(neighbours)
+	 	print openList
+	 	print closedList
+	 	# Then loop through the neighbours
+	 	for neighbour in neighbours:
+	 		Hcost = calculateH(neighbour[0], goal)
+	 		addedGCost = neighbour[1]
+	 		neighbour = [neighbour[0], currentGCost+addedGCost, Hcost, path]
+	 		x = neighbour[0][0]
+	 		y = neighbour[0][1]
 
-	#if end[0]!=minCostCell[0] and end[1]!=minCostCell[1]:
-	#	newStart = (minCostCell[0],minCostCell[1])
-	#	JumpPoint(newStart, end, dimension, maze)
+	 		# Check to see if the neighbour is traversable or if it's in the closedList (meaning we've already evaluated it and do not need to do so again.).
+	 		closed = False
+	 		length = len(closedList)-1
+	 		while length > 0:
+	 			if closedList[length][0] == (x,y):
+	 				closed=True
+	 				length=-1
+	 			length -=1
+
+	 		# If the node is traversable and is not in the closedList, we don't need to skip to the next neighbour.
+	 		if closed == False:
+	 			# Now we want to see if the new path to the neighbour is shorter (meaning it has a smaller G cost) or if the neighbor isn't in the openList.
+
+	 			newPathCost = currentGCost + 1 #gcost of previous node plus cost of travel
+	 			oldGCost = -1
+
+	 			# See if newpath is shorter than existing path by looking at the old Gcost.
+	 			for node in openList:
+	 				if node[0]==(x,y):
+	 				 	oldGCost = node[1]
+
+	 			# Check and see if node is in the openList
+	 			inOpen = False
+	 			for openNode in openList:
+	 				# if openNode[0] == current[0]:
+	 				inOpen = True
+
+	 			# If the new path to the node is less than the old cost or if it's in open
+	 			if newPathCost < oldGCost or inOpen:
+	 				# Remove it from the openList and set newNode
+	 				openList.remove(node)
+	 				closedList.append(node)
+	 				newNode = [node[0], newPathCost, node[2]]
+	 				#newNode.parent = currentNode
+
+	 			if inOpen == False:
+	 				openList.append(neighbour)
